d5c9dfab5bb93a61b66e76a6bae02ad684f5a273
OOmarQawass <omar12.qawas@gmail.com>
2025-06-16 10:04:15 +1200

    fixing routes.py

--- app/routes.py
+++ app/routes.py
@@ -1,8 +1,9 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template
 
 app = Flask(__name__)
 @app.route('/')
 def Parts(): return render_template('Parts Lists.html')
+
 
 if __name__ == '__main__':
     app.run(debug=True)
